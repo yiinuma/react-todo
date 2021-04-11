4eec5fc150448641cf2a6978d50949854aa485dc
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-04-11 20:48:53 +0900

    hello

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={contentStyle}>お元気ですか</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+export default App;
