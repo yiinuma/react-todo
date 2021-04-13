613bb799d786d4808ade43b75709604d1b8d50be
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-04-13 20:20:44 +0900

    props

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,10 @@
+const ColorfulMessage = (props) => {
+  const { color, children } = props;
+  const contentStyle = {
+    color: color,
+    fontSize: "18px"
+  };
+  return <p style={contentStyle}>{children}</p>;
+};
+
+export default ColorfulMessage;
