45ebbb153234f53bf3d571364d076d80a73d60fd
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-05-02 16:08:14 +0900

    52

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
-import ColorfulMessage from "./components/ColorfulMessage";
+import React, { useState } from "react";
+import "./styles.css";
 
-const App = () => {
-  const onClickButton = () => alert();
-
+export const App = () => {
+  const [incompleteTodos, setInCompleteTodos] = useState([
+    "ああああ",
+    "いいいい"
+  ]);
+  const [completeTodos, setCompleteTodos] = useState(["うううううう"]);
   return (
     <>
-      <h1 style={{ color: "red" }}>こんにちは</h1>
-      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
-      <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
-export default App;
