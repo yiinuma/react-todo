619ba8c3ef140508dac73c5860ec739062d0a2eb
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-05-02 17:04:03 +0900

    54

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,11 @@ export const App = () => {
     setInCompleteTodos(newTodos);
     setTodoText("");
   };
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setInCompleteTodos(newTodos);
+  };
   return (
     <>
       <div className="input-area">
@@ -28,12 +33,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
