6a3d7640d8366a90a92587d1cbff07d3643a4d25
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-05-03 18:57:40 +0900

    59

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,13 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるtodo5個までだよ〜。消化しろ〜。
+        </p>
+      )}
       <IncompleteTodos
         incompleteTodos={incompleteTodos}
         onClickComplete={onClickComplete}
